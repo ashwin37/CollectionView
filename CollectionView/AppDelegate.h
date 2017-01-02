6ef3ad813735caab2c523659e6f5779425322d36
ashwin37 <ashwint379@gmail.com>
2017-01-02 13:57:42 -0500

    Initial Commit

---
+++ CollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionView
+//
+//  Created by Ashwin Tallapaka on 1/2/17.
+//  Copyright © 2017 Ashwin Tallapaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
